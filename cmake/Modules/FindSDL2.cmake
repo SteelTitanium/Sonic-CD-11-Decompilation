c5e0bacac61e0ef14126661c0fb26fa969d7f4e2
SteelT <steeltitanium1@gmail.com>
2021-01-26 02:54:36 -0500

    Make library finding be more consistent

--- cmake/Modules/FindSDL2.cmake
+++ cmake/Modules/FindSDL2.cmake
@@ -135,7 +135,7 @@ endif()
 # MinGW needs an additional link flag, -mwindows
 # It's total link flags should look like -lmingw32 -lSDLmain -lSDL -mwindows
 if(MINGW)
-  set(MINGW32_LIBRARY mingw32 "-mwindows" CACHE STRING "link flags for MinGW")
+  set(MINGW32_LIBRARY -lmingw32 "-mwindows" CACHE STRING "link flags for MinGW")
 endif()
 
 if(SDL2_LIBRARY_TEMP)
